f38d033d35b22ab2a7884721f06c156cba98a31c
kiedunne <kie.dunne@gmail.com>
2019-02-08 00:38:19 +0000

    added array to pattern matching solution;

--- lib/fizzbuzz_pattern.ex
+++ lib/fizzbuzz_pattern.ex
@@ -1,12 +1,21 @@
 
 defmodule FizzbuzzerPattern do
 
-def convert (number) do
-  case {rem(number, 3), rem(number, 5)} do
-    {0,_} -> 'fizz'
-    {_,0} -> 'buzz'
-    {0,0} -> 'fizzbuzz'
+  def convert (number) do
+    case {rem(number, 3), rem(number, 5)} do
+      {0,0} -> 'fizzbuzz'
+      {0,_} -> 'fizz'
+      {_,0} -> 'buzz'
+      {_,_} -> number
+    end
   end
-end
+
+  def hundred_range do
+    Enum.map(1..100, &FizzbuzzerPattern.convert/1)
+  end
+
+  def enter_range(min, max) do
+    Enum.map(min..max, fn(num) -> FizzbuzzerPattern.convert(num) end)
+  end
 
 end
--- test/fizzbuzz_pattern_test.exs
+++ test/fizzbuzz_pattern_test.exs
@@ -9,4 +9,21 @@ defmodule ElixirFizzbuzzerPatternTest do
   test '5 is equal to buzz' do
     assert FizzbuzzerPattern.convert(5) == 'buzz'
   end
+
+  test "15 is equal to fizzbuzz" do
+    assert FizzbuzzerPattern.convert(15) == 'fizzbuzz'
+  end
+
+  test "2 is equal to 2" do
+    assert FizzbuzzerPattern.convert(2) == 2
+  end
+  test "fizzbuzzer array with range 1 to 100 consists of 100 elements" do
+    a = FizzbuzzerPattern.hundred_range
+    assert length(a) == 100
+  end
+
+  test "fizzbuzzer array with range 1 to 12 consists of 12 elements" do
+    a = FizzbuzzerPattern.enter_range(1,12)
+    assert length(a) == 12
+  end
 end
